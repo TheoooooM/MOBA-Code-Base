d57bd89884aee693d5a16fa12327b07336ddf483
BGauthier59 <g.baudinet@rubika-edu.com>
2022-11-22 00:04:56 +0100

    Interfaces events added V2

--- MOBA/Assets/Scripts/GlobalDelegates.cs
+++ MOBA/Assets/Scripts/GlobalDelegates.cs
@@ -5,6 +5,10 @@ public  class GlobalDelegates : MonoBehaviour
     public delegate void NoParameterDelegate();
     public delegate void BoolDelegate(bool b);
 
+    public delegate void ByteDelegate(bool b);
+
+    public delegate void UintDelegate(uint u);
+
     public delegate void FloatDelegate(float f);
 
     public delegate void Vector3Delegate(Vector3 v);
