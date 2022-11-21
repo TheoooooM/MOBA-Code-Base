cde0c6218eb1dbd0b9c811cba8fd1bb113ea94d9
Super Hubert <h.ramiandrasoa@rubika-edu.com>
2022-11-22 00:05:42 +0100

    IFOWShowable interface

--- MOBA/Assets/Scripts/Entities/FogOfWar/IFOWShowable.cs
+++ MOBA/Assets/Scripts/Entities/FogOfWar/IFOWShowable.cs
@@ -2,12 +2,18 @@ namespace Entities.FogOfWar
 {
     public interface IFOWShowable
     {
-        public void TryAddFOWViewable(IFOWViewable FOWWhichSee);
+        public bool CanShow();
+        public bool CanHide();
 
-        public void TryRemoveFOWViewable(IFOWViewable FOWWhichSee);
-
+        public void RequestSetCanShow(bool value);
+        public void SyncSetCanShowRPC(bool value);
+        public void SetCanShowRPC(bool value);
+        public void RequestSetCanHide(bool value);
+        public void SyncSetCanHideRPC(bool value);
+        public void SetCanHideRPC(bool value);
+        public void TryAddFOWViewable(uint viewableIndex);
         public void ShowElementsRPC();
-
+        public void TryRemoveFOWViewable(uint viewableIndex);
         public void HideElementsRPC();
     }
 }
