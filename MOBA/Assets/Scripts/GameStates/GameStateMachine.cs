4197e4053816379f23549c741e3c44537c2f936f
BGauthier59 <g.baudinet@rubika-edu.com>
2022-11-21 22:21:22 +0100

    clean project

---
+++ MOBA/Assets/Scripts/GameStates/GameStateMachine.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+
+namespace GameStates
+{
+    public class GameStateMachine : MonoBehaviourPun
+    {
+        
+    }
+}
+
+
