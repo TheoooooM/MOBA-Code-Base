b25d7dbc7955aa9b90b62af8de3e5dd0090e3f0d
ETU\d.harle <damien.harle@gmail.com>
2022-11-22 17:43:46 +0100

    Cam Controller

--- MOBA/Assets/Scripts/Controllers/CameraController.cs
+++ MOBA/Assets/Scripts/Controllers/CameraController.cs
@@ -1,16 +1,40 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 namespace Controllers
 {
-    public class CameraController : MonoBehaviour
+    public class CameraController : MonoBehaviour 
     {
+        //Script for the camera to follow the player
+        [SerializeField] private Transform player;
+        
+        [SerializeField] private float cameraDistance = 10f;
+        [SerializeField] private float cameraHeight = 5f;
+    
+        [SerializeField] private float cameraSpeed = 0.1f;
+        
+        private bool cameraLock = true;
+        private static CameraController mainCamera;
 
         private void Awake()
         {
             SetupInputMap();
             InputManager.PlayerMap.Camera.LockToggle.performed += OnToggleCameraLock;
         }
+
+        private void Start()
+        {
+            mainCamera = this;
+            //convert the camera Height to degrees
+        }
+
+        //create a static method to call from other scripts that links the transform of the player to the camera
+        public static void SetPlayer(Transform player)
+        {
+            mainCamera.player = player;
+        }
+        
 
         /// <summary>
         /// Setup the Camera InputMap of The Player inputs
@@ -19,6 +43,7 @@ namespace Controllers
         {
             InputManager.PlayerMap = new PlayerInputs();
             InputManager.PlayerMap.Enable();
+            Debug.Log("Input Map Set Up");
         }
 
         /// <summary>
@@ -27,6 +52,52 @@ namespace Controllers
         /// <param name="ctx"></param>
         void OnToggleCameraLock(InputAction.CallbackContext ctx)
         {
+            if (ctx.performed)
+            {
+                cameraLock = !cameraLock;
+                Debug.Log("Camera Lock Toggled");
+            }
+        }
+        
+        private void Update()
+        {
+            //if the player is not null
+            if (player !=  null)
+            {
+                //if the camera is locked the camera follows the player
+                if (cameraLock)
+                {
+                    transform.position = new Vector3(player.position.x - cameraDistance,
+                        player.position.y + cameraHeight, player.position.z - cameraDistance);
+                    transform.LookAt(player);
+                    Debug.Log("Camera Locked");
+                }
+                else
+                {
+                    //if the mouse is at the edge of the screen the camera moves
+                    if (Input.mousePosition.x >= Screen.width - 1)
+                    {
+                        transform.position += new Vector3(cameraSpeed, 0, -cameraSpeed);
+                    }
+
+                    if (Input.mousePosition.x <= 0)
+                    {
+                        transform.position += new Vector3(-cameraSpeed, 0, cameraSpeed);
+                    }
+
+                    if (Input.mousePosition.y >= Screen.height - 1)
+                    {
+                        transform.position += new Vector3(cameraSpeed, 0, cameraSpeed);
+                    }
+
+                    if (Input.mousePosition.y <= 0)
+                    {
+                        transform.position += new Vector3(-cameraSpeed, 0, -cameraSpeed);
+                    }
+
+                    Debug.Log("Camera Unlocked");
+                }
+            }
         }
     }
 }
--- MOBA/Assets/Scripts/Entities/Champion/Champion.cs
+++ MOBA/Assets/Scripts/Entities/Champion/Champion.cs
@@ -1,3 +1,6 @@
+using Controllers;
+using UnityEngine;
+
 namespace Entities.Champion
 {
     public partial class Champion : Entity
@@ -7,9 +10,14 @@ namespace Entities.Champion
         public byte attackAbilityIndex;
         public byte[] abilitiesIndexes = new byte[2];
         public byte ultimateAbilityIndex;
-        
-        protected override void OnStart() { }
+
+        protected override void OnStart() 
+        {
+            //set the camera to follow the champion using CameraController.LinkCameraToTransform(CameraController.mainCamera, transform);
+            CameraController.SetPlayer(transform); 
+        }
 
         protected override void OnUpdate() { }
+        
     }
 }
