2837fcaeaf1fb94f2f7c0e83826e28edd3137629
esdragor <64788862+esdragor@users.noreply.github.com>
2022-11-22 18:21:46 +0100

    AssignUIInventory
    ADD : Assign UIInventory placement when game launches

---
+++ MOBA/Assets/TEST/Test_entity.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using Entities;
+using UnityEngine;
+
+public class Test_entity : Entity
+{
+    protected override void OnStart()
+    {
+        
+    }
+
+    protected override void OnUpdate()
+    {
+        
+    }
+}
