c780267b208160ef2909ddbcb8b6b1b02f5977b8
Maxime Duflot <maximedft@outlook.com>
2022-11-23 13:33:23 +0100

    Fill ChampionResourceable / Fix ChampionActiveLifeable

--- MOBA/Assets/Scripts/Entities/Champion/ChampionResourceable.cs
+++ MOBA/Assets/Scripts/Entities/Champion/ChampionResourceable.cs
@@ -22,79 +22,163 @@ namespace Entities.Champion
             return currentResource / maxResource * 100;
         }
 
-        public void RequestSetMaxResource(float value) { }
+        public void RequestSetMaxResource(float value)
+        {
+            photonView.RPC("SetMaxResourceRPC", RpcTarget.MasterClient, value);
+        }
 
         [PunRPC]
-        public void SyncSetMaxResourceRPC(float value) { }
+        public void SyncSetMaxResourceRPC(float value)
+        {
+            maxResource = value;
+            OnSetMaxResourceFeedback?.Invoke(maxResource);
+        }
 
         [PunRPC]
-        public void SetMaxResourceRPC(float value) { }
+        public void SetMaxResourceRPC(float value)
+        {
+            maxResource = value;
+            OnSetMaxResource?.Invoke(maxResource);
+            photonView.RPC("SyncSetMaxResourceRPC", RpcTarget.All, maxResource);
+        }
 
         public event GlobalDelegates.FloatDelegate OnSetMaxResource;
         public event GlobalDelegates.FloatDelegate OnSetMaxResourceFeedback;
 
-        public void RequestIncreaseMaxResource(float amount) { }
+        public void RequestIncreaseMaxResource(float amount)
+        {
+            photonView.RPC("IncreaseMaxResourceRPC", RpcTarget.MasterClient, amount);
+        }
 
         [PunRPC]
-        public void SyncIncreaseMaxResourceRPC(float amount) { }
+        public void SyncIncreaseMaxResourceRPC(float amount)
+        {
+            maxResource = amount;
+            OnIncreaseMaxResourceFeedback?.Invoke(maxResource);
+        }
 
         [PunRPC]
-        public void IncreaseMaxResourceRPC(float amount) { }
+        public void IncreaseMaxResourceRPC(float amount)
+        {
+            maxResource += amount;
+            OnIncreaseMaxResource?.Invoke(maxResource);
+            photonView.RPC("SyncIncreaseMaxResourceRPC", RpcTarget.All, maxResource);
+        }
 
         public event GlobalDelegates.FloatDelegate OnIncreaseMaxResource;
         public event GlobalDelegates.FloatDelegate OnIncreaseMaxResourceFeedback;
 
-        public void RequestDecreaseMaxResource(float amount) { }
+        public void RequestDecreaseMaxResource(float amount)
+        {
+            photonView.RPC("DecreaseMaxResourceRPC", RpcTarget.MasterClient, amount);
+        }
 
         [PunRPC]
-        public void SyncDecreaseMaxResourceRPC(float amount) { }
+        public void SyncDecreaseMaxResourceRPC(float amount)
+        {
+            maxResource = amount;
+            OnDecreaseMaxResourceFeedback?.Invoke(maxResource);
+        }
 
         [PunRPC]
-        public void DecreaseMaxResourceRPC(float amount) { }
+        public void DecreaseMaxResourceRPC(float amount)
+        {
+            maxResource -= amount;
+            OnDecreaseMaxResource?.Invoke(maxResource);
+            photonView.RPC("SyncDecreaseMaxResourceRPC", RpcTarget.All, maxResource);
+        }
 
         public event GlobalDelegates.FloatDelegate OnDecreaseMaxResource;
         public event GlobalDelegates.FloatDelegate OnDecreaseMaxResourceFeedback;
 
-        public void RequestSetCurrentResource(float value) { }
+        public void RequestSetCurrentResource(float value)
+        {
+            photonView.RPC("SetCurrentResourceRPC", RpcTarget.MasterClient, value);
+        }
 
         [PunRPC]
-        public void SyncSetCurrentResourceRPC(float value) { }
+        public void SyncSetCurrentResourceRPC(float value)
+        {
+            currentResource = value;
+            OnSetCurrentResourceFeedback?.Invoke(currentResource);
+        }
 
         [PunRPC]
-        public void SetCurrentResourceRPC(float value) { }
+        public void SetCurrentResourceRPC(float value)
+        {
+            currentResource = value;
+            OnSetCurrentResource?.Invoke(currentResource);
+            photonView.RPC("SyncSetCurrentResourceRPC", RpcTarget.All, currentResource);
+        }
 
         public event GlobalDelegates.FloatDelegate OnSetCurrentResource;
         public event GlobalDelegates.FloatDelegate OnSetCurrentResourceFeedback;
 
-        public void RequestSetCurrentResourcePercent(float value) { }
+        public void RequestSetCurrentResourcePercent(float value)
+        {
+            photonView.RPC("SetCurrentResourcePercentRPC", RpcTarget.MasterClient, value);
+        }
 
         [PunRPC]
-        public void SyncSetCurrentResourcePercentRPC(float value) { }
+        public void SyncSetCurrentResourcePercentRPC(float value)
+        {
+            currentResource = value;
+            OnSetCurrentResourcePercentFeedback?.Invoke(currentResource);
+        }
 
         [PunRPC]
-        public void SetCurrentResourcePercentRPC(float value) { }
+        public void SetCurrentResourcePercentRPC(float value)
+        {
+            currentResource = value * maxResource;
+            OnSetCurrentResourcePercent?.Invoke(currentResource);
+            photonView.RPC("SyncSetCurrentResourcePercentRPC", RpcTarget.All, currentResource);
+        }
 
         public event GlobalDelegates.FloatDelegate OnSetCurrentResourcePercent;
         public event GlobalDelegates.FloatDelegate OnSetCurrentResourcePercentFeedback;
 
-        public void RequestIncreaseCurrentResource(float amount) { }
+        public void RequestIncreaseCurrentResource(float amount)
+        {
+            photonView.RPC("IncreaseCurrentResourceRPC", RpcTarget.MasterClient, amount);
+        }
 
         [PunRPC]
-        public void SyncIncreaseCurrentResourceRPC(float amount) { }
+        public void SyncIncreaseCurrentResourceRPC(float amount)
+        {
+            currentResource = amount;
+            OnIncreaseCurrentResourceFeedback?.Invoke(currentResource);
+        }
 
         [PunRPC]
-        public void IncreaseCurrentResourceRPC(float amount) { }
+        public void IncreaseCurrentResourceRPC(float amount)
+        {
+            currentResource += amount;
+            OnIncreaseCurrentResource?.Invoke(currentResource);
+            photonView.RPC("SyncIncreaseCurrentResourceRPC", RpcTarget.All, currentResource);
+        }
 
         public event GlobalDelegates.FloatDelegate OnIncreaseCurrentResource;
         public event GlobalDelegates.FloatDelegate OnIncreaseCurrentResourceFeedback;
 
-        public void RequestDecreaseCurrentResource(float amount) { }
+        public void RequestDecreaseCurrentResource(float amount)
+        {
+            photonView.RPC("DecreaseCurrentResourceRPC", RpcTarget.MasterClient, amount);
+        }
 
         [PunRPC]
-        public void SyncDecreaseCurrentResourceRPC(float amount) { }
+        public void SyncDecreaseCurrentResourceRPC(float amount)
+        {
+            currentResource = amount;
+            OnDecreaseCurrentResourceFeedback?.Invoke(currentResource);
+        }
 
         [PunRPC]
-        public void DecreaseCurrentResourceRPC(float amount) { }
+        public void DecreaseCurrentResourceRPC(float amount)
+        {
+            currentResource -= amount;
+            OnDecreaseCurrentResource?.Invoke(currentResource);
+            photonView.RPC("SyncDecreaseCurrentResourceRPC", RpcTarget.All, currentResource);
+        }
 
         public event GlobalDelegates.FloatDelegate OnDecreaseCurrentResource;
         public event GlobalDelegates.FloatDelegate OnDecreaseCurrentResourceFeedback;
